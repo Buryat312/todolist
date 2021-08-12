048841ef1b5bd979825a263b68fb8fb7d5bdf7cc
Sultanbek <sultanbek.asakeev@gmail.com>
2021-08-12 20:12:25 +0600

    Set up CRUD for task

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from tasks.models import Task
 from tasks.serializers import TaskSerializer
 
@@ -22,4 +23,30 @@ class TaskAPIView(APIView):
             task = Task.objects.create(body = body, estimated_finish_time = estimated_finish_time)
             serializer = TaskSerializer(instance=task)
             return Response(serializer.data)
+
         return Response({'detailed': serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+
+class TaskDetailAPIView(APIView):
+
+    def get(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(instance=task)
+        return Response(serializer.data)
+    
+    def put(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(instance=task, data=request.data)
+        if serializer.is_valid():
+            body = serializer.validated_data.get('body')
+            est = serializer.validated_data.get('estimated_finish_time')
+            task.body = body
+            task.estimated_finish_time = est
+            task.save()
+            return Response(serializer.data)
+
+        return Response({'detailed': serializer.errors}, status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        task = get_object_or_404(Task, id=id)
+        task.delete()
+        return Response({'detail':'success'}, status=status.HTTP_204_NO_CONTENT)
