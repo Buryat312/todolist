57594a5a5643657d7d1d75faa99fef17416abafd
Sultanbek <sultanbek.asakeev@gmail.com>
2021-08-14 18:52:36 +0600

    Updated

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import TaskAPIView, TaskDetailAPIView
 
 urlpatterns = [
-    path('tasks/', TaskDetailAPIView.as_view(), name='task_list_url'),
+    path('tasks/', TaskAPIView.as_view(), name='task_list_url'),
     path('tasks/<int:id>/', TaskDetailAPIView.as_view(), name='task_detail_url')
 ]
